838a98b9af2087fcd114e24ef38ec5133754463e
echaussidon <edmondchau@sfr.fr>
2020-12-14 17:03:25 +0100

    add linear regression

--- linear_fitting.py
+++ linear_fitting.py
@@ -1,17 +1,60 @@
 # coding: utf-8
 # Author : Edmond Chaussidon (CEA)
+#
+# Permet de faire une regression lineaire avec minuit rapidement
 
-class LeastSquares:  # override the class with a better one
-    def __init__(self, model, x, y, yerr):
-        self.model = model  # model predicts y for given x
+import numpy as np
+
+from iminuit import Minuit, describe
+from iminuit.util import make_func_code
+
+class LeastSquares:
+    def __init__(self, model, regulator, x, y):
+        self.model = model
+        self.regulator = regulator
         self.x = np.array(x)
         self.y = np.array(y)
-        self.yerr = np.array(yerr)
         self.func_code = make_func_code(describe(self.model)[1:])
 
-    def __call__(self, *par):  # par are a variable number of model parameters
+    def __call__(self, *par):
         ym = self.model(self.x, *par)
-        chi2 = sum((self.y - ym)**2/(self.yerr)**2)
+        sel = (self.y != 0)
+        chi2 = np.nansum(((self.y[sel] - ym[sel])**2/self.y[sel])) + self.regulator*(np.nanmean(ym[sel]) - 1)**2
         return chi2
 
-##TO DOOOo
+def regression_least_square(model, regulator, data_x, data_y, nbr_features, **dict_ini):
+    chisq = LeastSquares(model, regulator, data_x, data_y)
+    m = Minuit(chisq, print_level=1, forced_parameters=[f"a{i}" for i in range(0, nbr_features + 1)], **dict_ini)
+    m.migrad()
+    print(m.get_param_states())
+    return [m.values[f"a{i}"] for i in range(0, nbr_features + 1)]
+
+
+## EXEMPLE de fonction pour appeler la regression lineaire :
+#        * il faut modifier model
+#        * il faut modifier dict_ini
+#        * ici le code normalise les données uniquement sur la zone d'entrainement
+def make_linear_regressor(X, Y, keep_to_train, regulator=0.0):
+
+    nbr_features = X.shape[1]
+    print(f"[INFO] Number of features used : {nbr_features}")
+
+    X_train, Y_train = X[keep_to_train == 1], Y[keep_to_train == 1]
+
+    print("[WARNING :] We normalize and center features on the training footprint (don't forget to normalized also the non training footprint...)")
+    X = (X - X_train.mean())/X_train.std()
+    X_train = (X_train - X_train.mean())/X_train.std()
+    print(f"[INFO] Mean of Mean and Std training features : {X_train.mean().mean()} -- {X_train.std().mean()}")
+    print(f"[INFO] Mean and median of norm_target_density : {Y_train.mean()} -- {np.median(Y_train)}\n")
+
+    def model(x, *par): # Estimateur == modele utilise
+        return par[0]*np.ones(x.shape[0]) + np.array(par[1:]).dot(x.T)
+
+    dict_ini = {f'a{i}': 1 if i==0 else 0 for i in range(0, nbr_features + 1)}
+    dict_ini.update({f'error_a{i}' : 0.001 for i in range(0, nbr_features + 1)})
+    dict_ini.update({f'limit_a{i}': (-1, 2) if i==0 else (-1,1) for i in range(0, nbr_features + 1)})
+    dict_ini.update({'errordef':1}) #for leastsquare
+
+    param = regression_least_square(model, regulator, X_train, Y_train, nbr_features, **dict_ini)
+
+    return model(X, *param)
