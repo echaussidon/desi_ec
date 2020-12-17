cb715d2719ccf1abfa81366936ef912abc75434a
echaussidon <edmondchau@sfr.fr>
2020-12-17 19:23:14 +0100

    correction nbr_features --> nbr_params + ajout data_y_err ...

--- linear_regression.py
+++ linear_regression.py
@@ -9,35 +9,38 @@ from iminuit import Minuit, describe
 from iminuit.util import make_func_code
 
 class LeastSquares:
-    def __init__(self, model, regulator, x, y):
+    def __init__(self, model, regulator, x, y, y_err):
         self.model = model
         self.regulator = regulator
         self.x = np.array(x)
         self.y = np.array(y)
+        self.y_err = np.array(y_err)
         self.func_code = make_func_code(describe(self.model)[1:])
 
     def __call__(self, *par):
         ym = self.model(self.x, *par)
-        sel = (self.y != 0)
-        chi2 = np.nansum(((self.y[sel] - ym[sel])**2/self.y[sel])) + self.regulator*(np.nanmean(ym[sel]) - 1)**2
+        sel = (self.y_err != 0)
+        chi2 = np.nansum((self.y[sel] - ym[sel])**2/(self.y_err[sel])**2) + self.regulator*(np.nanmean(ym[sel]) - 1)**2
         return chi2
 
-def regression_least_square(model, regulator, data_x, data_y, nbr_features, **dict_ini):
-    chisq = LeastSquares(model, regulator, data_x, data_y)
-    m = Minuit(chisq, print_level=1, forced_parameters=[f"a{i}" for i in range(0, nbr_features + 1)], **dict_ini)
+def regression_least_square(model, regulator, data_x, data_y, data_y_err, nbr_params, **dict_ini):
+    chisq = LeastSquares(model, regulator, data_x, data_y, data_y_err)
+    m = Minuit(chisq, print_level=1, forced_parameters=[f"a{i}" for i in range(0, nbr_params)], **dict_ini)
     m.migrad()
     print(m.get_param_states())
-    return [m.values[f"a{i}"] for i in range(0, nbr_features + 1)]
+    return [m.values[f"a{i}"] for i in range(0, nbr_params)]
 
 
 ## EXEMPLE de fonction pour appeler la regression lineaire :
 #        * il faut modifier model
 #        * il faut modifier dict_ini
+#        * il faut adapter les erreurs
 #        * ici le code normalise les données uniquement sur la zone d'entrainement
 def make_linear_regressor(X, Y, keep_to_train, regulator=0.0):
 
     nbr_features = X.shape[1]
     print(f"[INFO] Number of features used : {nbr_features}")
+    nbr_params = nbr_features + 1
 
     X_train, Y_train = X[keep_to_train == 1], Y[keep_to_train == 1]
 
@@ -50,11 +53,13 @@ def make_linear_regressor(X, Y, keep_to_train, regulator=0.0):
     def model(x, *par): # Estimateur == modele utilise
         return par[0]*np.ones(x.shape[0]) + np.array(par[1:]).dot(x.T)
 
-    dict_ini = {f'a{i}': 1 if i==0 else 0 for i in range(0, nbr_features + 1)}
-    dict_ini.update({f'error_a{i}' : 0.001 for i in range(0, nbr_features + 1)})
-    dict_ini.update({f'limit_a{i}': (-1, 2) if i==0 else (-1,1) for i in range(0, nbr_features + 1)})
+    dict_ini = {f'a{i}': 1 if i==0 else 0 for i in range(0, nbr_params)}
+    dict_ini.update({f'error_a{i}' : 0.001 for i in range(0, nbr_params)})
+    dict_ini.update({f'limit_a{i}': (-1, 2) if i==0 else (-1,1) for i in range(0, nbr_params)})
     dict_ini.update({'errordef':1}) #for leastsquare
 
-    param = regression_least_square(model, regulator, X_train, Y_train, nbr_features, **dict_ini)
+    Y_err_train = np.sqrt(Y_train)
+
+    param = regression_least_square(model, regulator, X_train, Y_train, Y_err_train, nbr_features, **dict_ini)
 
     return model(X, *param)
